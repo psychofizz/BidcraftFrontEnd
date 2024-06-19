8e881484c8ae26bad276430171c0e7d6b7a987aa
lricdiaz <lricdiaza@gmail.com>
2024-06-18 21:52:43 -0600

    Agregando LandingPage

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import './App.css';
 import Login from './Pages/Login';
 import Form from './Pages/Form';
+import LandingPage from './Pages/LandingPage';
 import {BrowserRouter,Routes,Route} from "react-router-dom";
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path="/Login" element={<Login/>}></Route>
           <Route path="/Form" element={<Form/>}></Route>
+          <Route path="/" element={<LandingPage/>}></Route>
         </Routes>
         </BrowserRouter>
         
