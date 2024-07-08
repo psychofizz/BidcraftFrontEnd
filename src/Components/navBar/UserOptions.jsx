8f7a4520c250696d3ffab7166577788b2029bf24
psychofizz <psychofizz@pm.me>
2024-07-08 13:08:02 -0600

    Cambios de Ricardo y Avance en usuario

---
+++ src/Components/navBar/UserOptions.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const UserOptions = () => {
+  return (
+    <div>
+      <div className="flex">
+        <span>
+          <div className="w-20 h-20 rounded-full mr-4">
+            <img src="https://picsum.photos/200" alt="" srcset="" />
+          </div>
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default UserOptions;
