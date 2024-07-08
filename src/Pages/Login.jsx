deec0235520cc3ba876d2ee9b5d200de287b76e3
psychofizz <psychofizz@pm.me>
2024-07-07 22:05:00 -0600

    Merge conflict resolution

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,19 +1,8 @@
-<<<<<<< HEAD
-import React, { useState } from 'react';
-import mainLogo from '../img/bidLogo.png'
-import ComponentsInput from '../Components/input'
-import { toast } from 'react-toastify';
-import { useNavigate } from 'react-router-dom';
-
-
-
-=======
 import React, { useState } from "react";
 import mainLogo from "../img/bidLogo.png";
 import ComponentsInput from "../Components/input";
 import { toast } from "react-toastify";
 import { useNavigate } from "react-router-dom";
->>>>>>> develop
 
 function Login() {
   // const users3 = {
@@ -66,7 +55,6 @@ function Login() {
         if (!response.ok) {
           switch (response.status) {
             case 400:
-             
               toast.warning("Datos invalidos o datos no existentes");
               break;
             default:
@@ -169,15 +157,9 @@ function Login() {
         </div>
         <div className="text-sm text-gray-700 py-5 px-10">
           <p>
-<<<<<<< HEAD
-            <p >Ayuda    </p>
-            <p >Privacidad    </p>
-            <p >Terminos    </p>
-=======
             <a href="">Ayuda </a>
             <a href="">Privacidad </a>
             <a href="">Terminos </a>
->>>>>>> develop
           </p>
         </div>
       </div>
