46c877cbf3b8096499afdda107d7e49db89d0885
beginLyra <barcelona147jn@gmail.com>
2024-07-09 11:40:01 -0600

    arregando problema de tw elements

--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState,useEffect } from 'react';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faStar } from "@fortawesome/free-solid-svg-icons";
 import {
@@ -6,9 +6,11 @@ import {
     initTWE,
   } from "tw-elements";
   
-  initTWE({ Tab });
+ 
 function Profile() {
-    
+  useEffect(() => {
+    initTWE({ Tab });
+  })
 
     return (
 <div>
