6f46c1ae6bf04e4bad837a9f58b03453ed3a7947
beginLyra <barcelona147jn@gmail.com>
2024-07-09 17:58:11 -0600

    gitMerge branch 'develop' into develop_josue

--- src/Pages/HomeTest.jsx
+++ src/Pages/HomeTest.jsx
@@ -1,38 +1,99 @@
-import React from "react";
+import React,{useEffect,useState} from "react";
 import MainNavbar from "../Components/navBar/mainNavbar";
 import Footer from "../Components/page-essentials/Footer";
 import AuctionItem from "../Components/auction/auctionItem";
+import { useNavigate } from "react-router-dom";
 
 const HomeTest = () => {
+//Validando que  este el token en el local storage
+const [userInfo, setUserInfo] = useState([]);
+const [productInfo, setProductInfo] = useState([]);
+const jwt = JSON.parse(localStorage.getItem("token"));
+
+const navigate = useNavigate();
+useEffect(() => {
+
+//Estableciendo las rutas protegidas
+ if (jwt === null) {
+   navigate('/login')
+ } else {
+    obtenInfo(jwt);
+    obtenProducto();
+
+  }
+
+ }, [jwt])
+
+//Esto nos srive para obtener informacion del usuario
+
+const obtenInfo = async (token) => {
+  try {
+    const response = await fetch("http://127.0.0.1:8000/api/auth/test/", {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json",
+      },
+    });
+    if (!response.ok) {
+    } else {
+      const dataUser = await response.json();
+      console.log(dataUser)
+      setUserInfo(dataUser);
+ 
+    }
+  } catch (error) {}
+};
+
+//Esto nos srive para obtener todos los productos
+const obtenProducto = async (token) => {
+  try {
+    const response = await fetch("http://127.0.0.1:8000/api/auction/show/all/", {
+      method: "GET",
+      headers: {
+      
+        "Content-Type": "application/json",
+      },
+    });
+    if (!response.ok) {
+    } else {
+      const data = await response.json();
+      setProductInfo(data)
+      console.log(productInfo)
+     
+    }
+  } catch (error) {}
+};
+
   return (
     <div className="min-h-screen">
-      <MainNavbar />
-      <div className="grid grid-cols-3 m-2 p-2 shadow-2 space-x-1 space-y-1">
-        <AuctionItem
-          title="Camara"
-          d
-          description="Yo se lo que tengo"
-          price="500"
-          endDate="2019"
-          num_of_favorites="12"
-        ></AuctionItem>
+      <MainNavbar userName={userInfo ? userInfo.first_name : null}/>
+      <div className="grid grid-cols-2 m-2 p-2 shadow-2 space-x-1 space-y-1">
+        
 
-        <AuctionItem
-          title="Camara"
-          d
-          description="Yo se lo que tengo"
-          price="500"
-          endDate="2019"
+      {productInfo.length > 0 ? (
+  <section>
+    {productInfo.map(producto => (
+      
+       <div key={producto.auction_id}>
+          <AuctionItem
+          title={producto.name}
+          description={producto.description}
+          price={producto.starting_price}
+          endDate={producto.end_time}
           num_of_favorites="12"
         ></AuctionItem>
-        <AuctionItem
-          title="Camara"
-          d
-          description="Yo se lo que tengo"
-          price="500"
-          endDate="2019"
-          num_of_favorites="12"
-        ></AuctionItem>
+       </div>
+      
+     
+    ))}
+    </section>
+) : (
+  <p>No hay productos disponibles.</p>
+)}
+     
+        
+
       </div>
     </div>
   );
