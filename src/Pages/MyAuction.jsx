f25e8d37bcd309da67476c1a19d5ddeef4bcda10
beginLyra <barcelona147jn@gmail.com>
2024-08-07 10:26:15 -0600

    agregando bids hechas en una subasta y loading page en my auctions

--- src/Pages/MyAuction.jsx
+++ src/Pages/MyAuction.jsx
@@ -1,47 +1,51 @@
 import React, { useEffect, useState, useCallback } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import axios from "axios";
+
+// Componentes
+import MainNavbar from "../Components/navBar/mainNavbar";
+import CategoriesBar from "../Components/navBar/CategoriesBar";
 import AuctionInfo from "../Components/activeAuction/auctionInfo";
 import CardOffert from "../Components/myActiveAuction/cardOffert";
 import CardSeller from "../Components/activeAuction/cardSeller";
-import { useNavigate, useParams } from "react-router-dom";
 import Footer from "../Components/page-essentials/Footer";
-import MainNavbar from "../Components/navBar/mainNavbar";
-import CategoriesBar from "../Components/navBar/CategoriesBar";
+import { toast } from "react-toastify";
+import LoadingPage from "../Components/loading";
 
-import axios from "axios";
+
 
 function Auction() {
+  const [isActive, setIsActive] = useState(false)
+
   const [data, setData] = useState({});
+  const [bidData, setBidData] = useState({});
   const [isFavorite, setIsFavorite] = useState(false);
-  const jwt = JSON.parse(localStorage.getItem("token"));
-  const navigate = useNavigate();
-  const { id: auctionId } = useParams();
-  const userId = JSON.parse(localStorage.getItem("User"));
   const [status, setStatus] = useState(null);
   const [loading, setLoading] = useState(true);
+  const jwt = JSON.parse(localStorage.getItem("token"));
+  const userId = JSON.parse(localStorage.getItem("User"));
+  const navigate = useNavigate()   //-------------------Obtener la información de la subasta-----------------------------
+  const { id: auctionId } = useParams();
 
-  //-----------------------------------------------------Este nos ayudara a ver si el usuario ya envio su verificacion------------------
-  const StatusUser = async () => {
-    try {
-      const response = await axios.get(
-        `${process.env.REACT_APP_API_URL}/api/kyc/detail/`,
-        {
-          headers: {
-            Authorization: `Bearer ${JSON.parse(
-              localStorage.getItem("token")
-            )}`,
-            "Content-Type": "application/json",
-          },
-        }
-      );
 
-      setStatus(response.data.status.status_id);
-    } catch (error) {
-    } finally {
-      setLoading(false); // Termina la carga después de obtener el estado
-    }
-  };
-  //-------------------Obtener la información de la subasta-----------------------------
+
   const infoAuction = useCallback(async () => {
+    try {
+        const response = await axios.get(`${process.env.REACT_APP_API_URL}/api/auction/show/one/${auctionId}/`);
+        setData(response.data);
+
+        setIsActive(response.data.is_active)
+        
+
+    } catch (error) {
+        console.error('Error al obtener la subasta:', error);
+    }
+}, [auctionId]);
+;
+
+
+  // Función para obtener información de la subasta
+  const fetchAuctionData = useCallback(async () => {
     try {
       const response = await axios.get(
         `${process.env.REACT_APP_API_URL}/api/auction/show/one/${auctionId}/`
@@ -52,93 +56,122 @@ function Auction() {
     }
   }, [auctionId]);
 
-  //-----------------------------Lógica de favoritos-----------------------------------------------//
-
-  const stateFavorite = useCallback(async () => {
-    const dataFavorite = {
-      user: userId.id,
-      auction: data.auction_id,
-    };
-
-    if (!data.auction_id) return;
-
+  // Función para obtener las ofertas de la subasta
+  const fetchBidInfo = useCallback(async () => {
     try {
-      const response = await axios.get(
-        `${process.env.REACT_APP_API_URL}/api/favorites/check/${dataFavorite.auction}/`,
+      const bidInfo = await axios.get(
+        `${process.env.REACT_APP_API_URL}/api/bids/auction/${auctionId}/`,
         {
           headers: {
-            Authorization: `Bearer ${JSON.parse(
-              localStorage.getItem("token")
-            )}`, // Incluir el token en los headers
+            Authorization: `Bearer ${jwt}`,
+          },
+        }
+      );
+   
+      setBidData(bidInfo.data.data);
+    } catch (error) {
+      toast.error(error)
+    }
+  }, [auctionId, jwt]);
+
+  // Función para obtener el estado de verificación del usuario
+  const fetchUserStatus = useCallback(async () => {
+    try {
+      const response = await axios.get(
+        `${process.env.REACT_APP_API_URL}/api/kyc/detail/`,
+        {
+          headers: {
+            Authorization: `Bearer ${jwt}`,
+            "Content-Type": "application/json",
           },
         }
       );
 
+      setStatus(response.data.status.status_id);
+    } catch (error) {
+      console.error("Error al obtener el estado del usuario:", error);
+    } finally {
+      setLoading(false);
+    }
+  }, [jwt]);
+
+  // Función para comprobar y alternar el estado de favoritos
+  const checkAndToggleFavorite = useCallback(async () => {
+    if (!data.auction_id) return;
+
+    try {
+      // Comprobar estado de favoritos
+      const response = await axios.get(
+        `${process.env.REACT_APP_API_URL}/api/favorites/check/${data.auction_id}/`,
+        {
+          headers: {
+            Authorization: `Bearer ${jwt}`,
+          },
+        }
+      );
       setIsFavorite(response.data.exists);
     } catch (error) {
       console.error("Error en favorito:", error);
     }
-  }, [data.auction_id, userId.id]);
+  }, [data.auction_id, jwt]);
 
   const toggleFavorite = async () => {
     const dataFavorite = {
       user: userId.id,
       auction: data.auction_id,
     };
-    try {
-      const token = JSON.parse(localStorage.getItem("token"));
 
+    try {
       if (isFavorite) {
         await axios.delete(
           `${process.env.REACT_APP_API_URL}/api/favorites/delete/one/auction/${dataFavorite.auction}/`,
           {
             headers: {
-              Authorization: `Bearer ${token}`, // Incluir el token en los headers
+              Authorization: `Bearer ${jwt}`,
             },
           }
         );
       } else {
         await axios.post(
           `${process.env.REACT_APP_API_URL}/api/favorites/auction/${dataFavorite.auction}/`,
-          {}, // Cuerpo de la solicitud (vacío en este caso)
+          {},
           {
             headers: {
-              Authorization: `Bearer ${token}`, // Incluir el token en los headers
+              Authorization: `Bearer ${jwt}`,
             },
           }
         );
       }
       setIsFavorite(!isFavorite);
     } catch (error) {
-      console.error("Error en favorito:", error);
+      console.error("Error al alternar favorito:", error);
     }
   };
 
-  //----------------------------------------------------------------------------------------------------------------------
+  // useEffect para la carga inicial de datos
   useEffect(() => {
-    StatusUser();
-    // Estableciendo las rutas protegidas
-    if (jwt === null) {
+    if (!jwt) {
       navigate("/login");
     } else {
+      fetchUserStatus();
+      fetchAuctionData();
+      fetchBidInfo();
       infoAuction();
     }
-  }, [jwt, navigate, auctionId, infoAuction]);
+  }, [jwt, navigate, auctionId, fetchAuctionData, fetchBidInfo, fetchUserStatus,infoAuction]);
 
+  // useEffect para verificar el estado de favoritos
   useEffect(() => {
-    if (data.auction_id) {
-      stateFavorite();
-    }
-  }, [data.auction_id, stateFavorite]);
+    checkAndToggleFavorite();
+  }, [checkAndToggleFavorite]);
 
-  //---------------------------------------------------------------------------------------------------------
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex flex-col min-h-screen bg-bidcraft-grey">
       <MainNavbar />
       <CategoriesBar />
-      <div className="flex-grow w-full grid grid-cols-1 lg:grid-cols-4 bg-bidcraft-grey">
+      <div className=" h-[100%] bg-bidcraft-grey min-h-screen">
         {data.name && data.description ? (
-          <>
+          <div className="flex-grow w-full grid grid-cols-1 lg:grid-cols-4 bg-bidcraft-grey" >
             <div className="col-span-2 lg:col-span-3">
               <AuctionInfo
                 name={data.name}
@@ -149,6 +182,7 @@ function Auction() {
                 toggleFavorite={toggleFavorite}
                 isFavorite={isFavorite}
                 end_time={data.end_time}
+                is_active={isActive}
               />
             </div>
             <div className="md:mr-10 md:mt-4 p-2">
@@ -157,16 +191,21 @@ function Auction() {
                   lastOffert={data.highest_bid}
                   idAuction={data.auction_id}
                   jwt={jwt}
-                  updateAuction={infoAuction}
+                  updateAuction={fetchAuctionData}
                   loading={loading}
                   status={status}
+                  bidData={bidData}
                 />
-                <CardSeller username={data.seller.username} nameSeller={data.seller.first_name + " " + data.seller.last_name} sellerId={data.seller.id}  />
+                <CardSeller
+                  username={data.seller.username}
+                  nameSeller={`${data.seller.first_name} ${data.seller.last_name}`}
+                  sellerId={data.seller.id}
+                />
               </div>
             </div>
-          </>
+          </div>
         ) : (
-          <div className="w-full h-full ">Cargando...</div>
+          <div className="grid place-content-center min-h-screen"><LoadingPage /></div>
         )}
       </div>
       <Footer />
