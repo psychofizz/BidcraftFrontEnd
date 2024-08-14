f73af434bd7ee59f13950aa0512a3a773777d67f
beginLyra <barcelona147jn@gmail.com>
2024-08-13 20:55:35 -0600

    agregando factura

--- src/Components/profile/AuctionList.jsx
+++ src/Components/profile/AuctionList.jsx
@@ -167,7 +167,7 @@ const AuctionList = ({ auctions, loadingMyAuction, openModal }) => {
     {/* Aquí puedes renderizar los detalles de la factura */}
     {loading ? (
     
-            <LoadingPage />
+            <div className=' rounded-lg p-5 bg-bidcraft-grey'><LoadingPage /></div>
         
     ) : factura === null ? (
         <div className="text-center py-6">
@@ -176,7 +176,7 @@ const AuctionList = ({ auctions, loadingMyAuction, openModal }) => {
     ) : factura ? (
         <div className="bg-white p-6 rounded-lg shadow-lg max-w-4xl mx-auto max-h-[90vh] overflow-auto">
             <h2 className="text-3xl font-bold mb-6 text-center text-blue-600">Factura de Compra</h2>
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mb-6">
+            <div className="grid grid-cols-1 md:grid-cols-1 gap-6 mb-6">
                 <div className="p-4 border border-gray-200 rounded-lg">
                     <h3 className="text-xl font-semibold text-gray-700 mb-4">Detalles de la Compra</h3>
                     <p className="text-gray-600"><span className="font-semibold">ID de Compra:</span> {factura.purchase_id || 'N/A'}</p>
